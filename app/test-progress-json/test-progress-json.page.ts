7bfd347389e692f6e02f87207839fc453a2b4366
aru1702 <muhamadaldy17@gmail.com>
2019-06-19 21:11:40 +0700

    19/06/2019
    2

--- app/test-progress-json/test-progress-json.page.ts
+++ app/test-progress-json/test-progress-json.page.ts
@@ -37,6 +37,10 @@ export class TestProgressJsonPage implements OnInit {
    * field variable
    */
   items: any = [];
+  
+  // test array
+  dummy: any = [];
+  dummy2: any = [];
 
   /**
    * Contructor of test-progress-json page
@@ -47,10 +51,81 @@ export class TestProgressJsonPage implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.main();
+    this.main();  
+  }
+
+  async sortFunction (myArray) {
+    await myArray.sort((a, b) => {
+      let dateA = new Date(a['date']), dateB = new Date(b['date']);
+  
+      if (dateB > dateA)
+        return 1;
+      if (dateB < dateA)
+        return -1;
+
+      return 0;
+    });
+
+    return myArray;
+  }
+
+  async timeFunction () {
+    let a;
+
+    a = "2019-03-08 16:32:00";
+    this.getTime(a);
+
+    a = "2019-06-19 14:22:23";
+    this.getTime(a);
+
+    a = "2019-07-08 13:00:00"
+    this.getTime(a);
+
+    a = "2019-08-01 13:00:00"
+    this.getTime(a);
+
+    a = "2019-08-14 13:00:00"
+    this.getTime(a);
+
+    a = "2019-09-12 13:00:00"
+    this.getTime(a);
+
+    a = "2019-10-03 13:00:00"
+    this.getTime(a);
   }
 
   async main () {
+    ///////////////////
+    // time function //
+    ///////////////////
+    await this.timeFunction();
+    
+    console.log("Before sort");
+    console.log(this.dummy);
+
+    /**
+     * copy array value
+     * this method is used because:
+     * - if passing the array variable then that array will be execute,
+     *   not array in variable inside the function
+     * - if copy from one to another, it will only reference (pointing) to
+     *   the same array, concept of object oriented programming
+     */
+    let dummyArray: any = [];
+    for (let i = 0 ; i < this.dummy.length ; i++) {
+      dummyArray[i] = this.dummy[i];
+    }
+
+    await this.sortFunction(dummyArray).then((value) => {
+      this.dummy2 = value;
+    });
+
+    console.log("After sort");
+    console.log(this.dummy2);
+
+    ////////////////////////
+    // dispenser function //
+    ////////////////////////
     let myJson = await this.getAllDispenser();
 
     for (let i = 0 ; i < myJson.length ; i++) {
@@ -111,6 +186,47 @@ export class TestProgressJsonPage implements OnInit {
     }
 
     return returnJson;
+  }
+
+  getTime (time) {
+    // time passed is String, construct into Date format
+    // time example from json: "2019-03-08 16:32:00"
+    // format: YEAR-MONTH-DATEOFMONTH HOUR:MINUTE:SECOND
+    
+    // split into DATE form and HOUR form
+    let splitTime = time.split(" ");
+
+    let resultDate = splitTime[0];
+    let resultHour = splitTime[1];
+
+    // split DATE into YEAR, MONTH, and DATEOFMONTH
+    let splitDate = resultDate.split("-");
+
+    let resultYear = splitDate[0];
+    let resultMonth = splitDate[1] - 1;
+    let resultDateOfMonth = splitDate[2];
+
+    // split HOUR into HOUR, MINUTE, and SECOND
+    let splitHour = resultHour.split(":");
+
+    let resultHourC = splitHour[0];
+    let resultMinute = splitHour[1];
+    let resultSecond = splitHour[2];
+
+    // now we get every component to construct date from String
+    let newDate = new Date(
+      resultYear,
+      resultMonth,
+      resultDateOfMonth,
+      resultHourC,
+      resultMinute,
+      resultSecond,
+      0
+    );
+
+    // console.log(newDate);
+    let a = {'date': newDate};
+    this.dummy.push(a);
   }
 
 }
